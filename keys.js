c9b6babea4323ce2b1eb12b205dd0c1a42f81e9e
Kevin Malone <zhaokevinma@gmail.com>
2019-09-04 08:41:17 -0400

    movieApp completed

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.spotify = {
-    id: ProcessingInstruction.env.SPOTIFY_ID,
-    secret: ProcessingInstruction.env.SPOTIFY_SECRET
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
 
 exports.omdb = {
