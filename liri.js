c58db08eb06661fd52782aabf7a83411d89aa87f
Kevin Malone <zhaokevinma@gmail.com>
2019-09-05 15:36:54 -0400

    do-what-it-says feature completed

--- liri.js
+++ liri.js
@@ -10,11 +10,40 @@ var spotifyApp = require("./spotifyApp.js");
 // To require bandsInTownApp function from movieApp.js
 var bandsInTownApp = require("./bandsInTownApp.js");
 
+//To import File System npm package
+var fs = require("fs");
+
 // To store userCommand
 var userCommand = process.argv[2];
 
 // To store and process userInput
 var userInput = process.argv.splice(3, process.argv.length).join('+');
+
+// doWhatItSays function
+function doWhatItSays () {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
+            var dataArr = data.split(",");
+
+            switch (dataArr[0]) {
+                case "movie-this":
+                    var movie = dataArr[1].slice(1,-1);
+                    movieApp(movie);
+                    break;
+                case "spotify-this-song":
+                    var song = dataArr[1].slice(1,-1);
+                    spotifyApp(song);
+                    break;
+                case "concert-this":
+                    var artist = dataArr[1].slice(1,-1);
+                    spotifyApp(artist);
+                    break;
+            }
+        }
+    });
+}
 
 // Switch case conditions to choose apps
 switch (userCommand) {
@@ -34,6 +63,9 @@ switch (userCommand) {
         break;
     case "concert-this":
         bandsInTownApp(userInput);
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
         break;
     default:
         console.log("Please type 'node liri.js help' for instructions");
